6252f3236eba4f4b5a5d8da9d64e9fea4e4e6df4
rahoelr <rahul.rahmatullah@mail.ugm.ac.id>
2023-04-10 01:58:23 +0700

    Update : Bahasa dan App Widget

---
+++ app/src/main/java/com/example/rahoolfood/MySensor.java
@@ -0,0 +1,14 @@
+package com.example.rahoolfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MySensor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_sensor);
+    }
+}
