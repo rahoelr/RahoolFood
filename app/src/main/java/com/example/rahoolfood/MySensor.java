017e8c5f5a90a73d5c51a4b73cf29a88066a1dfe
rahoelr <rahul.rahmatullah@mail.ugm.ac.id>
2023-04-10 15:51:35 +0700

    Update : Sensor Proximity

--- app/src/main/java/com/example/rahoolfood/MySensor.java
+++ app/src/main/java/com/example/rahoolfood/MySensor.java
@@ -2,13 +2,57 @@ package com.example.rahoolfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MySensor extends AppCompatActivity {
+public class MySensor extends AppCompatActivity implements SensorEventListener {
+
+    private TextView tv_sensor;
+    private SensorManager sensorManager;
+    private Sensor tempSensor;
+    private Boolean isTemperatureSensorAvail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_sensor);
+
+        tv_sensor = findViewById(R.id.tv_sensor);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if(sensorManager != null){
+            Sensor proxiSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+            if(proxiSensor != null){
+                sensorManager.registerListener(this, proxiSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            }
+        }
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_PROXIMITY){
+            tv_sensor.setText(sensorEvent.values[0]+" mm");
+
+            if(sensorEvent.values[0] > 0){
+                Toast.makeText(this, "Object is Far", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Object is Near", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
 }
